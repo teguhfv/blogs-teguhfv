39c20d5bec4ee039bf79dea10762a786b2fcf029
Teguhfv <teguhfv@gmail.com>
2018-12-06 10:34:55 +0800

    test post

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,42 @@
 import React from 'react'
-import { Link } from 'gatsby'
-
+import { Link, graphql } from 'gatsby'
+import './post.css';
 import Layout from '../components/layout'
-import Image from '../components/image'
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-       <Link to="/new/">new</Link>
-       <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = (props) => {
+  const postList = props.data.allMarkdownRemark;
+  return (
+    <Layout>
+      {postList.edges.map(({ node }, i) => (
+        <Link to={node.fields.slug} className="link" >
+          <div className="post-list">
+            <h1>{node.frontmatter.title}</h1>
+            <span>{node.frontmatter.date}</span>
+            <p>{node.excerpt}</p>
+          </div>
+        </Link>
+      ))}
+    </Layout>
+  )
+}
 
-export default IndexPage
+export default IndexPage;
+
+export const listQuery = graphql`
+  query ListQuery {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          fields{
+            slug
+          }
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM Do YYYY")
+            title
+          }
+        }
+      }
+    }
+  }
+`
