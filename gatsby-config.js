2f4df587f848af8652b77f590df8535ced78c6b6
Teguhfv <teguhfv@gmail.com>
2018-12-06 09:47:19 +0800

    transformer-remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,8 +25,14 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    `gatsby-transformer-remark`,
+    'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: "pages",
+      },
+    },
   ],
 }
