2a5a513b95681d8af5e8fbfcb62007aa7d1a5192
Teguhfv <teguhfv@gmail.com>
2018-12-06 10:28:20 +0800

    test post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,19 +2,25 @@ import React from 'react';
 import Layout from '../components/layout';
 import { graphql } from 'gatsby'
 
-function BlogPost() {
+function BlogPost(props) {
+
+    const post = props.data.markdownRemark;
+    const { title } = post.frontmatter;
+
     return (
         <Layout>
             <div>
-                hello post
-        </div>
+                <h1>{title}</h1>
+                <div dangerouslySetInnerHTML={{ __html: post.html }} />
+            </div>
         </Layout>
     )
 }
 
-export default BlogPost
+export default BlogPost;
 
-const query = graphql`
+export const query = graphql`
+
  query PostQuery($slug: String!) {
      markdownRemark(fields: { slug: { eq: $slug } }) {
        html
@@ -23,3 +29,4 @@ const query = graphql`
         description
        }
    }
+}`
