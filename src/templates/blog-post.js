0e00fe7f8d8a130b70bf7e25831095bc2b4f270a
Teguhfv <teguhfv@gmail.com>
2018-12-06 10:24:22 +0800

    test post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import Layout from '../components/layout';
+import { graphql } from 'gatsby'
 
 function BlogPost() {
     return (
         <Layout>
             <div>
-              lorem ipsum
+                hello post
         </div>
         </Layout>
     )
 }
 
 export default BlogPost
+
+const query = graphql`
+ query PostQuery($slug: String!) {
+     markdownRemark(fields: { slug: { eq: $slug } }) {
+       html
+       frontmatter {
+        title
+        description
+       }
+   }
